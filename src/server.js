d3e7f480c1a1d13cfa8e43a233c480062eac11aa
Joey Freund <joeyfreund@gmail.com>
2016-06-23 11:18:29 -0400

    change GET products (on the server) to shuffle the products

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+var shuffle = require('knuth-shuffle').knuthShuffle
 var express = require('express');
 var app = express();
 
@@ -13,12 +14,12 @@ app.get('/api/:version/products', function (req, res) {
   console.log(`Got an API call (API version ${req.params.version})`)
 
   res.json({
-    items: [
+    items: shuffle([
       {brand: 'Chloe', product: 'Blue Denim Dress', image: 'https://res.cloudinary.com/ssenseweb/image/upload/b_white,c_lpad,g_south,h_1086,w_724/c_scale,h_560/v489/162338F052002_1.jpg'},
       {brand: 'Palm Angels', product: 'Blue USA Cap', image: 'https://res.cloudinary.com/ssenseweb/image/upload/b_white,c_lpad,g_south,h_1086,w_724/c_scale,h_560/v488/161695M139003_1.jpg'},
       {brand: 'Blackmeans', product: 'Green Check Shirt', image: 'https://res.cloudinary.com/ssenseweb/image/upload/b_white,c_lpad,g_south,h_1086,w_724/c_scale,h_560/v488/161625M192001_1.jpg'},
       {brand: 'Kenzo', product: 'Black Tiger T-Shirt', image: 'https://res.cloudinary.com/ssenseweb/image/upload/b_white,c_lpad,g_south,h_1086,w_724/c_scale,h_560/v488/162387F110001_1.jpg'}
-    ]
+    ])
   });
 
 });
