8a6431946f9cbebb4b7ee17f5a8bf22781d86fbe
Joey Freund <joeyfreund@gmail.com>
2016-06-22 22:37:52 -0400

    pass a callback as property

--- src/client.js
+++ src/client.js
@@ -4,11 +4,16 @@ import React from 'react';
 import {render} from 'react-dom';
 import HelloWorld from './components/hello.js';
 
+const myClickHandler = (e) => {
+	e.preventDefault();
+	alert('You clicked me.')
+};
+
 
 render(
   <div>
     <h1>Welcome</h1>
-    <HelloWorld name="Dave"/>
+    <HelloWorld name="Dave" callback={ myClickHandler }/>
   </div>,
   document.getElementById('root')
 );
--- src/components/hello.js
+++ src/components/hello.js
@@ -5,7 +5,7 @@ export default class HelloWorld extends React.Component {
 
   render() {
     return (
-      <p>
+      <p onClick={this.props.callback}>
         Hello, {this.props.name}! I am a React component
       </p>
     );
