a1afd6fc45bbfae42e565a047773bf1024ac98e3
Joey Freund <joeyfreund@gmail.com>
2016-06-23 09:45:41 -0400

    load data via API call, when a React component mounts

--- src/components/catalog_items.js
+++ src/components/catalog_items.js
@@ -2,19 +2,27 @@ import React from 'react';
 import CatalogItem from './catalog_item.js';
 
 
+
 export default class CatalogItems extends React.Component {
 
+  componentWillMount() {
+    console.log('Need to make an API call to get data for catalog items ...');
+
+    require('isomorphic-fetch');
+
+    fetch('/api/v1/products')
+      .then(res => res.json())
+      .then(json => { console.log('Loaded data:', json); })
+      .then(() => { console.log('Re-rendering ...'); this.render(); })
+      .catch(err => { console.log('ERROR', err); });
+  }
+
+
   render() {
-
-  	let css = require('./catalog_items.css');
-  	console.log(css);
-  	console.log(css.container);
-
-
     return (
       <div>
       	{this.props.items.map(
-      		(item, i) => { 
+      		(item, i) => {
       			return (
       				<div key={i} style={{ float: 'left' }}>
       					<CatalogItem  {...item} />
