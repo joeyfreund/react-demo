2a49cded853a39b56fc2119aac6aa50b17801b78
Joey Freund <joeyfreund@gmail.com>
2016-07-13 20:36:48 -0400

    update fixme comment

--- src/client.js
+++ src/client.js
@@ -14,19 +14,16 @@ import ProductPage from './components/product_page.js';
 
 // FIXME: Currently there is one major issue with our application:
 //        - React and Redux are not wired together correctly.
-//        - We should really use react-redux's connect function to do the wiring
-//          for us.
+//          We should really use react-redux's connect() function to do the
+//          wiring for us.
 //        - Unfortunately, we did not get a chance to into the details of
 //          the connect function, and tried to wire things together manually.
-//        - We are currently hacking two things:
-//          1. We made the Redux store a global object (instead of letting
-//             react-redux pass it from the Provider to all the components down
-//             the tree)
-//          2. In the Catalog component, we subscribe to the store and call
-//             this.forceUpdate(), instead of this.render().
-//             This is very bad, because it means that React will actually
-//             modify the DOM every time (instead of using the render() method
-//             which does a smart diff and touches the DOM only if it needs to).
+//        - We are currently using the following hack:
+//          In the Catalog component, we subscribe to the store and call
+//          this.forceUpdate(), instead of this.render().
+//          This is very bad, because it means that React will actually
+//          modify the DOM every time (instead of using the render() method
+//          which does a smart diff and touches the DOM only if it needs to).
 
 
 var store = createStore(reducer);
